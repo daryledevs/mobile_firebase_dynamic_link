94a565748edf9c4a088de0d1ce53e609c700847e
Daryle Karl De Vera <darylekarl.devera@gmail.com>
2022-10-25 01:02:54 +0800

    Generate referral url with user's id in the link

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
-import React, { useEffect } from 'react'
+import {StyleSheet, Text, View, Button} from 'react-native';
+import React, {useEffect} from 'react';
 import dynamicLinks from '@react-native-firebase/dynamic-links';
 
 async function buildLink() {
-  const link = await dynamicLinks().buildLink({
-    link: 'https://firebasereferralreward.page.link',
+  const STRING = "invitedBy";
+  const SENDER_UID = 'USER1234';
+  const link = await dynamicLinks().buildShortLink({
+    link: `https://firebase-referral-reward.com/?${STRING}=${SENDER_UID}`,
     // domainUriPrefix is created in your Firebase console
-    domainUriPrefix: 'https://referralrewardsapplication.page.link/DQbR',
+    domainUriPrefix: 'https://referralrewardsapplication.page.link',
+    android: {
+      packageName: 'com.rn_firebase_referral_rewards',
+    },
     // optional setup which updates Firebase analytics campaign
     // "banner". This also needs setting up before hand
   });
@@ -15,25 +20,34 @@ async function buildLink() {
 }
 
 const App = () => {
-
   useEffect(() => {
+
+    function getParameterFromUrl(url, parm) {
+      var re = new RegExp('.*[?&]' + parm + '=([^&]+)(&|$)');
+      var match = url.match(re);
+      console.log(match)
+      return match ? match[1] : '';
+    }
+
     dynamicLinks()
       .getInitialLink()
       .then(link => {
-        if (link.url === 'https://firebase-referral-reward.com') {
-          // ...set initial route as offers screen
-          console.log('Link: ', link);
+        const url = link?.url;
+        console.log(link)
+        if(url){
+          const id = getParameterFromUrl(url, 'invitedBy');
+          console.log("id: ", id)
         }
       });
   }, []);
 
   return (
     <View>
-      <Button title='Test' onPress={buildLink} />
+      <Button title="Test" onPress={buildLink} />
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
