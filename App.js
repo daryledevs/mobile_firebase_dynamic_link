c1e372ef696c46482c18fbd9e0d369d72da2fc93
Daryle Karl De Vera <darylekarl.devera@gmail.com>
2022-10-24 21:16:03 +0800

    Print incoming url when dynamic link is being open

--- App.js
+++ App.js
@@ -1,23 +1,32 @@
 import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import dynamicLinks from '@react-native-firebase/dynamic-links';
 
 async function buildLink() {
   const link = await dynamicLinks().buildLink({
     link: 'https://firebasereferralreward.page.link',
     // domainUriPrefix is created in your Firebase console
-    domainUriPrefix: 'https://firebasereferralreward.page.link/76UZ',
+    domainUriPrefix: 'https://referralrewardsapplication.page.link/DQbR',
     // optional setup which updates Firebase analytics campaign
     // "banner". This also needs setting up before hand
-    analytics: {
-      campaign: 'banner',
-    },
   });
-  console.log(link)
+  console.log(link);
   return link;
 }
 
 const App = () => {
+
+  useEffect(() => {
+    dynamicLinks()
+      .getInitialLink()
+      .then(link => {
+        if (link.url === 'https://firebase-referral-reward.com') {
+          // ...set initial route as offers screen
+          console.log('Link: ', link);
+        }
+      });
+  }, []);
+
   return (
     <View>
       <Button title='Test' onPress={buildLink} />
