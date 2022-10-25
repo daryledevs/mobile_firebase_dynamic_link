30c41272c82a467b794bebdfbc960e6a0acd1489
Daryle Karl De Vera <darylekarl.devera@gmail.com>
2022-10-25 09:42:58 +0800

    Unmount dynamicLink when the link is obtained

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import {StyleSheet, Text, View, Button} from 'react-native';
 import React, {useEffect} from 'react';
-import dynamicLinks from '@react-native-firebase/dynamic-links';
+import { firebase } from '@react-native-firebase/dynamic-links';
 
 async function buildLink() {
   const STRING = "invitedBy";
   const SENDER_UID = 'USER1234';
-  const link = await dynamicLinks().buildShortLink({
+  const link = await firebase.dynamicLinks().buildShortLink({
     link: `https://firebase-referral-reward.com/?${STRING}=${SENDER_UID}`,
     // domainUriPrefix is created in your Firebase console
     domainUriPrefix: 'https://referralrewardsapplication.page.link',
@@ -20,7 +20,8 @@ async function buildLink() {
 }
 
 const App = () => {
-  useEffect(() => {
+
+  useEffect( async () => {
 
     function getParameterFromUrl(url, parm) {
       var re = new RegExp('.*[?&]' + parm + '=([^&]+)(&|$)');
@@ -29,16 +30,25 @@ const App = () => {
       return match ? match[1] : '';
     }
 
-    dynamicLinks()
-      .getInitialLink()
-      .then(link => {
-        const url = link?.url;
-        console.log(link)
-        if(url){
-          const id = getParameterFromUrl(url, 'invitedBy');
-          console.log("id: ", id)
-        }
-      });
+   try {
+    // mount
+     const unsubscribe = firebase
+       .dynamicLinks()
+       .getInitialLink()
+       .then(link => {
+         const url = link?.url;
+        //  console.log(link);
+         if (url) {
+           const id = getParameterFromUrl(url, 'invitedBy');
+           console.log('id: ', id);
+         }
+       });
+      
+      // unmount
+      return unsubscribe;
+   } catch (error) {
+    console.log(error)
+   }
   }, []);
 
   return (
